48e32e4d81e269d9f8a31274382f83bb5cc1b725
omer-alt <wafomer912@gmail.com>
2022-07-19 20:07:02 +0200

    initialisation of Banner section

---
+++ src/components/banner.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../styles/banner.css'
+import banner from '../assets/firsImg.jpeg'
+import {Container, Col, Row,Card,Button } from 'react-bootstrap'
+
+
+const Banner = () => {
+  return (
+    <Container fluid >
+        <Row >
+            <img src={banner} className="bgProfil" alt="backgroundProfil"/> 
+            <div className='bannerOverlay col-6' >
+                <span className='bTitle'>Web App Developper</span>
+                <h1 className='bName mt-lg-1 mb-2'>Omer-alt FOTSO</h1>
+                <p className='bTexte'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod
+                    tempor incididunt ut dolor the sit amet consectetur adipisicing
+                    elit sed do consectetur labore et dolo
+                </p>
+                <Button className='btEffect mt-lg-5' variant="success" size='lg'>VIEW MORE</Button>
+            </div>
+            <div className='col-6'></div>
+        </Row>
+    </Container>
+  )
+}
+
+export default Banner
---
+++ src/components/navbar.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import '../styles/navBar.css'
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useParams,
+  } from "react-router-dom";
+import { Navbar,Nav,NavDropdown,Form,FormControl,Button,Container } from 'react-bootstrap'
+import logo from '../assets/logo.png'
+// import Home from './Home';
+// import AboutUs from './AboutUs';
+// import ContactUs from './ContactUs';
+
+const NavBar = () =>{ 
+    
+    return(
+        <div className='backnavColor'>
+            <Container >
+                <div className="row">
+                    <Router>
+                        <Navbar variant='dark' expand="lg" sticky="top">
+                            <div className='col-lg-2'>
+                                <Navbar.Brand href="#home" >                                    
+                                    <img className='navLogo' alt="logo" src={logo} />
+                                    <span className="text-success dim">&#46;</span>                                  
+                                </Navbar.Brand>
+                            </div>                        
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                <Nav></Nav>
+                                <Nav >                                   
+                                    <Nav.Link href="/">Home</Nav.Link>
+                                    <Nav.Link href="/contact-us">About Us</Nav.Link>
+                                    <Nav.Link href="/service">Service</Nav.Link>
+                                    <Nav.Link href="/experience">Experience</Nav.Link>
+                                    <Nav.Link href="/porfolio">Portfolio</Nav.Link>
+                                    <Nav.Link href="/blog">Blog</Nav.Link>
+                                    <Nav.Link href="/contact">Contact</Nav.Link>                                 
+                                </Nav>
+                            </Navbar.Collapse>
+                        
+                        </Navbar>
+                        <br />
+                        <Routes>
+                            <Route exact path="/">
+                                        {/* <Home /> */}
+                            </Route>
+                            <Route path="/about-us">
+                                        {/* <AboutUs /> */}
+                            </Route>
+                            <Route path="/contact-us">
+                                        {/* <ContactUs /> */}
+                            </Route>
+                        </Routes>
+                    </Router>              
+                        
+                </div>
+            </Container>
+        </div>
+    )  
+    
+}
+export default NavBar;
