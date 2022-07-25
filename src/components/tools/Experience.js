1e8a152f006f179c92b3f10b8bc412dba9c3aedc
omer-alt <wafomer912@gmail.com>
2022-07-25 02:18:23 +0200

    Experience section animation

--- src/components/tools/Experience.js
+++ src/components/tools/Experience.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { Card } from 'react-bootstrap'
+import  AOS  from 'aos';
+import "aos/dist/aos.css";
 import '../../styles/experiences.css'
 
 const Experience = ({experience}) => {
+
+    useEffect(()=>{
+        AOS.init({duration: 1000})
+    },[])
+
   return (
     <div className='col-12 col-sm-6 col-md-6 mt-3'>
-      <Card className='cardExperience' >
+      <Card className='cardExperience' data-aos="flip-up" >
         <Card.Body >
             <Card.Title className='cardExperienceTitle pt-2'>
               {experience.title}
               <p className='cardPeriod pt-2'>{experience.period}</p>
             </Card.Title>
-          <hr/>
-          <Card.Text className='cardDescription'>
-            {experience.description}
-          </Card.Text>
+          <hr className='ms-5'/>
+            <Card.Text className='cardDescription'>
+                {experience.description}
+            </Card.Text>
         </Card.Body>
       </Card>
     </div>
--- src/components/tools/Service.js
+++ src/components/tools/Service.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import {Card} from 'react-bootstrap'
+import  AOS  from 'aos';
+import "aos/dist/aos.css";
 import '../../styles/service.css'
 
 const SIngleService = ({service}) => {
     // pour importer l'image de la carte concernee
   const chemin = require("../../assets/"+service.image)
 
+  useEffect(()=>{
+    AOS.init({duration: 1000})
+  },[])
+
   return (
     <div className='col-lg-4 col-md-6 col-sm-6 col-12 mt-3'>
-      <Card className='cardService'>
+      <Card className='cardService' data-aos="fade-up">
         <Card.Img className='cardServiceImg ' variant="bottom" src={chemin} />
         <Card.Body className='cardServiceBody'>
           <Card.Title className='cardServiceTitle'>{service.title}</Card.Title>
